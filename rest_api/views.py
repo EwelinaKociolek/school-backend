c2cfbe9a89a51bed184c2ac9c31835ef2ae02ce3
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-03-07 21:45:16 +0100

    school-backend first steps

---
+++ rest_api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Exam, Task
+from .serializers import ExamSerializer, TaskSerializer
+
+
+class ExamView():
+    queryset = Exam.objects.all()
+    serializers_class = ExamSerializer
+
+
+class TaskView():
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
