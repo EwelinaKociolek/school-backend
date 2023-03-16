9ab38042151466274464c7a066a1eb901cf8b5e5
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-03-16 18:05:53 +0100

    add permissions, edit urls, views

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,5 +1,14 @@
-# from rest_framework.routers import DefaultRouter
-#
-# router = DefaultRouter()
-#
-# router.register("exams")
+from .views import ExamView, TaskView
+from rest_framework.routers import DefaultRouter
+from rest_framework_extensions.routers import NestedRouterMixin
+
+class NestedDefaultRouter(NestedRouterMixin, DefaultRouter):
+    pass
+
+router = NestedDefaultRouter()
+
+router.register("exams", ExamView, basename="exams").register("tasks", TaskView, basename="exam-tasks", parents_query_lookups=["exam"])
+
+router.register("tasks", TaskView, basename="tasks")
+
+urlpatterns = router.urls
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.viewsets import ModelViewSet
+
 from .models import Exam, Task
+from .permissions import IsExamOwnerOrReadOnly, IsOwnerOrReadOnly
 from .serializers import ExamSerializer, TaskSerializer
 
 
-class ExamView():
+class ExamView(ModelViewSet):
     queryset = Exam.objects.all()
-    serializers_class = ExamSerializer
+    serializer_class = ExamSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 
-class TaskView():
+class TaskView(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsExamOwnerOrReadOnly)
